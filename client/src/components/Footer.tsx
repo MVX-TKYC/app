e597a081bb4debecae54b7bd547416b913102b14
Theo F <theo.farnole@gmail.com>
2023-10-13 15:18:01 +0200

    fix typo in footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function FooterSection() {
     return (
         <footer className={style.footer}>
             <img src="/img/tkyc_logo.png" alt="Logo TKYC" />
-            <p>is made to show the potential of processes Data from blockchain for the MultiversX Hackathon</p>
+            <p>was created to demonstrate the potential of blockchain-based data processes for the MultiversX Hackathon.</p>
         </footer>
     );
 }
