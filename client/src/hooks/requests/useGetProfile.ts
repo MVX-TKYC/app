26360ecf7f19d68687fed6325cd2c156b7b22ff9
Theo F <theo.farnole@gmail.com>
2023-10-17 15:34:25 +0200

    update env REACT_APP_API_URL

--- client/src/hooks/requests/useGetProfile.ts
+++ client/src/hooks/requests/useGetProfile.ts
@@ -9,7 +9,7 @@ interface Output {
 }
 
 export default function useGetProfile(address: string): Output | undefined {
-    const url = (process.env.REACT_API_URL ?? "http://127.0.0.1:8000") + "/profile/" + address;
+    const url = (process.env.REACT_APP_API_URL ?? "http://127.0.0.1:8000") + "/profile/" + address;
     const { data } = useGenericAPICall<any>(url);
 
     if (data != undefined) {
