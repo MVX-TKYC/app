c1b93a17283604acfbd66fd0ac42390399eaf3a7
Theo F <theo.farnole@gmail.com>
2023-10-17 10:30:48 +0200

    ADD option to read transactions from disk instead of network (make development faster)

--- server/README.md
+++ server/README.md
@@ -15,3 +15,14 @@ uvicorn main:app --reload
 ### Documentation
 
 When running the server, the Swagger documentation is available at [http://127.0.0.1:8000/docs](http://127.0.0.1:8000/docs)
+
+### Speeding up development
+
+
+To enable the reading of transactions from local storage instead of the network, you should create a .env file and specify the path to the folder using the following variable:
+
+```
+TRANSACTIONS_CACHE_PATH="path_to_folder"
+```
+
+Within this designated folder, you should store transaction data in individual files named after their corresponding addresses. For example, you may have a file named `erd1hdejmz9shgquyk6k85da5r9rvmk49p4lts8z9pvc3fd5kku78qrsrusact.json`, which contains all the relevant transactions.
--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ from converter import NoTokenError, flatten_transactions
 from profile import calculate_profile
 import os
 import json
+from dotenv import dotenv_values
 
 app = FastAPI()
 
@@ -15,7 +16,7 @@ async def root():
 @app.get("/profile/{address}")
 async def address(address, response: Response ):    
 
-    transactions = get_all_transactions(address)
+    transactions = get_transactions(address)
 
     try:
 
@@ -34,3 +35,25 @@ async def address(address, response: Response ):
             "address": address,
             "error": "Address do not have tokens."
         }
+
+# Use cache or download transaction
+def get_transactions(address):
+
+    config = dotenv_values(".env")
+    base_path_key = "TRANSACTIONS_CACHE_PATH"
+
+    if base_path_key in config:
+        base_path = config[base_path_key]
+        path = os.path.join(base_path, address+".json")
+
+        if os.path.exists(path):
+            print("cache found")
+
+            file = open(path, "r")
+            transactions = json.load(file)
+            file.close()  
+
+            return transactions  
+    
+
+    return get_all_transactions(address)
